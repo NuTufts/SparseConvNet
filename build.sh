ca4c71087d8b8324ca8433ed42f633d196c06a85
twongjirad <taritree.wongjirad@gmail.com>
2019-04-06 17:37:29 -0400

    slight mod for python3

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 rm -rf build/ dist/ sparseconvnet.egg-info
-python setup.py install
-python examples/hello-world.py
+python3 setup.py install
+python3 examples/hello-world.py
