bbc44dcd90cd51bdc87e2848632922d540b2990c
twongjirad <taritree.wongjirad@gmail.com>
2019-07-01 15:13:56 -0400

    comment out cuda vars

--- build.sh
+++ build.sh
@@ -4,10 +4,11 @@
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
-CUDA_HOME=/usr/local/cuda
-CUDA_BIN=${CUDA_HOME}/bin
-CUDA_LIB=${CUDA_HOME}/lib
 
+# Set these to help find cuda
+#CUDA_HOME=/usr/local/cuda
+#CUDA_BIN=${CUDA_HOME}/bin
+#CUDA_LIB=${CUDA_HOME}/lib
 
 rm -rf build/ dist/ sparseconvnet.egg-info
 python setup.py install
