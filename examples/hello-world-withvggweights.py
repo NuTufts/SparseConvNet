97fac42ef6f7c069933ed03e8d746e87847b6d62
taritree <taritree.wongjirad@gmail.com>
2021-07-06 18:10:30 -0400

    x

--- examples/hello-world-withvggweights.py
+++ examples/hello-world-withvggweights.py
@@ -35,10 +35,8 @@ vgg_layers = [ ['C',64,],
                ['C',128],
                ['MP',3,2],
                ['C',256],
-               ['C',24],
+               ['C',32],
                ['MP',3,2] ]
-               #['C',512],
-               #['C',512],
                #['MP',3,2],               
                #['C',512],
                #['C',512],
@@ -55,10 +53,10 @@ default_layers = [['C', 8],
                
 model = scn.Sequential().add(
     scn.SparseVggNet(2, 1, vgg_layers )
-).add(
-    scn.SubmanifoldConvolution(2, 24, 32, 3, False)
-).add(
-    scn.BatchNormReLU(32)
+#).add(
+#scn.SubmanifoldConvolution(2, 24, 32, 3, False)
+#).add(
+#    scn.BatchNormReLU(32)
 ).add(
     scn.SparseToDense(2, 32)
 ).to(device)
@@ -67,7 +65,7 @@ print(model)
 par_dict = {}
 for par in model.named_parameters():
     print("--------------------------------------------")
-    print(par[0],par[1].shape)
+    print("setting [",par[0],par[1].shape,"]")
     if "weight" in par[0] and len(par[0].split("."))==3 and len(par[1].shape)==4:
         layerid1 = int(par[0].split(".")[0])
         layerid2 = int(par[0].split(".")[1])
@@ -78,7 +76,6 @@ for par in model.named_parameters():
             continue
         vggweight = np.transpose( np_vggweight, (2,3,1,0) )
         vggweight = torch.from_numpy( vggweight.reshape( (9,1,vggweight.shape[2],vggweight.shape[3]) ) ).to(device)
-        print(type(par[1]))
         print(par[0],par[1].shape,'-->',vgglayername,vggweight.shape)
         par_dict[par[0]] = vggweight[:,:,:par[1].shape[2],:par[1].shape[3]]
     else:
@@ -90,8 +87,8 @@ for par in model.named_parameters():
     if "bias" in par[0] and len(par[0].split("."))==3 and len(par[1].shape)==1:
         par_dict[par[0].replace("bias","running_mean")] = torch.zeros( (par[1].shape[0]) )
 #print(par_dict)
-par_dict["2.running_mean"] = torch.zeros( (32) )
-par_dict["2.running_var"]  = torch.ones( (32) )
+#par_dict["2.running_mean"] = torch.zeros( (32) )
+#par_dict["2.running_var"]  = torch.ones( (32) )
 model.load_state_dict(par_dict)
     
 # output will be 10x10
@@ -125,15 +122,12 @@ locations = torch.LongTensor(locations)
 features = torch.FloatTensor(features).to(device)
 
 input = input_layer([locations,features])
-print('Input SparseConvNetTensor:', input)
+print('Input SparseConvNetTensor:', input.features.shape)
 output = model(input)
 
 # Output is 2x32x10x10: our minibatch has 2 samples, the network has 32 output
 # feature planes, and 10x10 is the spatial size of the output.
 print('Output SparseConvNetTensor:', output)
-
-
-
 
 # Alternatively:
 # Create Nx3 and Nx1 vectors to encode the messages above using BLInputLayer:
@@ -150,7 +144,7 @@ batch=scn.prepare_BLInput(batch)
 batch[1]=batch[1].to(device)
     
 input = bl_input_layer(batch)
-print('Input SparseConvNetTensor:', input)
+print('Input SparseConvNetTensor:', input.features.shape)
 output = model(input)
 
 # Output is 2x32x10x10: our minibatch has 2 samples, the network has 32 output
